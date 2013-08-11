204dc82ac20819b01cd63ae1ee29578488360d5f
Ankit Gupta <ankit.gupta8898@gmail.com>
2013-08-11 21:06:05 +0100

    Simple reverse method

--- app/scripts/controllers/userController.js
+++ app/scripts/controllers/userController.js
@@ -4,4 +4,9 @@ angular.module('angularDemoApp')
   .controller('UsercontrollerCtrl', function ($scope,datum) {
     $scope.user = {name: "Ankit"};
     $scope.factory = datum;
+
+    $scope.reversedArray = function(){
+    	var v = {message: "From Method", bb: $scope.factory.arr.reverse()};
+    	return v;
+    }
   });
