1c1cf967adc021dee8c6ee268ea3ae3cc31ac459
Ankit Gupta <ankit.gupta8898@gmail.com>
2013-08-12 10:30:37 +0100

    User json

--- app/scripts/services/datum.js
+++ app/scripts/services/datum.js
@@ -5,8 +5,10 @@ angular.module('angularDemoApp')
     // Service logic
     // ...
 
-    var r = {arr: [1,2,3,4,5,6,7], name: "From factory!"};
-    return r;
+    //var r = {arr: [1,2,3,4,5,6,7], name: "From factory!"};
+    var users = [{name: 'Ankit', age: 26},
+                 {name: 'Ankita', age: 26}]
+    return users;
     // Public API here
   
   });
